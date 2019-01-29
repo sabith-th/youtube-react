002bcec00bfaabc73f431580d287424a41b57c83
SabithHaneef <sabith.th.07@gmail.com>
2019-01-29 22:11:42 +0530

    made VidePreview customizable to be horizontal or vertical grid

--- src/components/VideoPreview/__tests__/VideoPreview.unit.test.js
+++ src/components/VideoPreview/__tests__/VideoPreview.unit.test.js
@@ -3,8 +3,12 @@ import React from "react";
 import { VideoPreview } from "../VideoPreview";
 
 describe("Video Preview", () => {
-  test("renders", () => {
+  test("renders vertically", () => {
     const wrapper = shallow(<VideoPreview />);
+    expect(wrapper).toMatchSnapshot();
+  });
+  test("renders horizontally", () => {
+    const wrapper = shallow(<VideoPreview horizontal={true} />);
     expect(wrapper).toMatchSnapshot();
   });
 });
