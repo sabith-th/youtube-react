7bb7ca1a357baea3629670a07a294a4507e34c53
SabithHaneef <sabith.th.07@gmail.com>
2019-02-03 19:42:01 +0530

    setup redux, redux-saga

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import "semantic-ui-css/semantic.min.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import { configureStore } from "./store/configureStore";
+
+const store = configureStore();
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
 
---
+++ src/store/reducers/index.js
@@ -0,0 +1,6 @@
+export default (state={}, action) => {
+  switch (action.typ) {
+    default:
+      return state;
+  }
+}
