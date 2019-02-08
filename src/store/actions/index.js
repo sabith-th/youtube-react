293f2ea2751e85471a62b4c5aaebe6bf514f36ea
SabithHaneef <sabith.th.07@gmail.com>
2019-02-08 19:57:22 +0530

    add code for generating action names for api requests

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,3 +1,18 @@
+export const REQUEST = "REQUEST";
+export const SUCCESS = "SUCCESS";
+export const FAILURE = "FAILURE";
+
+export const createRequestTypes = base => {
+  if (!base) {
+    throw new Error("Cannot create request type with base = '' or base = null");
+  }
+
+  return [REQUEST, SUCCESS, FAILURE].reduce((acc, type) => {
+    acc[type] = `${base}_${type}`;
+    return acc;
+  }, {});
+};
+
 export const createAction = (type, payload = {}) => {
   return {
     type,
