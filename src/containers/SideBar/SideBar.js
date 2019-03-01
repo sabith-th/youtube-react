215160e181181a998157a819576213b078b112cd
SabithHaneef <sabith.th.07@gmail.com>
2019-03-01 16:00:38 +0530

    make sidebar items links

--- src/containers/SideBar/SideBar.js
+++ src/containers/SideBar/SideBar.js
@@ -3,15 +3,15 @@ import { Divider, Menu } from "semantic-ui-react";
 import "./SideBar.scss";
 import { SideBarFooter } from "./SideBarFooter/SideBarFooter";
 import { SideBarHeader } from "./SideBarHeader/SideBarHeader";
-import { SideBarItem } from "./SideBarItem/SideBarItem";
+import SideBarItem from "./SideBarItem/SideBarItem";
 import { Subscriptions } from "./Subscriptions/Subscriptions";
 
 export class SideBar extends React.Component {
   render() {
     return (
       <Menu borderless vertical stackable fixed="left" className="side-nav">
-        <SideBarItem highlight={true} label="Home" icon="home" />
-        <SideBarItem label="Trending" icon="fire" />
+        <SideBarItem highlight={true} label="Home" icon="home" path="/" />
+        <SideBarItem label="Trending" icon="fire" path="/feed/trending" />
         <SideBarItem label="Followers" icon="spy" />
         <Divider />
         <SideBarHeader title="Library" />
